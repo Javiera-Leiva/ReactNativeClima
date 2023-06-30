d46f99d50486dfef6d056475f87c839db6926aee
Javiera-Leiva <javiealeiva026@gmail.com>
2023-06-29 20:32:37 -0600

    Modificacion del componente del App.js y llamado del componente ClimaScreen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import ClimaScreen from './src/ClimaScreen'
 
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+            <ClimaScreen />
     </View>
   )
 }
