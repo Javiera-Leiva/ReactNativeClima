5feb0ba5f9f6ab4f732cbe3898e8cd4906541181
Javiera-Leiva <javiealeiva026@gmail.com>
2023-06-29 20:03:43 -0600

    Definición del componente ClimaScreen e importaciones necesarias

---
+++ src/ClimaScreen.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet} from 'react-native';
+import axios from 'axios';
+
+
+const ClimaScreen = () => {
+
+}
+
+export default ClimaScreen;
