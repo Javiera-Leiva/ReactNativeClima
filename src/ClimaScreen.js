a898e24e325fb1da0ac3619490d40a461d4d9ac1
Javiera-Leiva <javiealeiva026@gmail.com>
2023-06-29 20:30:23 -0600

    Retorno de la lista de paises con su estado del clima aplicando estilos

--- src/ClimaScreen.js
+++ src/ClimaScreen.js
@@ -33,8 +33,46 @@ const ClimaScreen = () => {
       </View>
     );
   }
+  return (
+    <View style={styles.container}>
+      {climaData.map((clima, index) => (
+        <View key={index} style={styles.weatherContainer}>
+          <Text style={styles.text}>Ubicación: {clima.location.name}, {clima.location.region}, {clima.location.country} </Text>
+          <Text style={styles.text}>Temperatura: {clima.current.temp_c}°C</Text>
+          <Text style={styles.text}>Condición: {clima.current.condition.text}</Text>
+          <Text style={styles.text}>Presión atmosférica: {clima.current.pressure_mb} mb</Text>
+        </View>
+      ))}
+    </View>
+  );
+};
 
-
-}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F2E6F7',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  cargando: {
+    fontSize: 18,
+    color: '#000',
+    textAlign: 'center',
+  },
+  weatherContainer: {
+    marginBottom: 50,
+    padding: 40,
+    backgroundColor: '#D7BDE2',
+    borderRadius: 20,
+    shadowColor: '#AC00F4',
+    shadowOpacity: 0.5,
+    elevation: 5,
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 5,
+    color: '#000',
+  },
+});
 
 export default ClimaScreen;
