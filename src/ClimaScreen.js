46b1fa5aae8e825ac8230c0d0cac2a1085085a93
Javiera-Leiva <javiealeiva026@gmail.com>
2023-06-29 20:26:01 -0600

    Declaracion de arreglo y configuracion de API

--- src/ClimaScreen.js
+++ src/ClimaScreen.js
@@ -5,6 +5,27 @@ import axios from 'axios';
 
 const ClimaScreen = () => {
 
+  const [climaData, setClimaData] = useState(null);
+  // Lista de países que deseo mostrar
+  const countries = ['Nicaragua', 'Costa Rica','Panama',];
+
+  useEffect(() => {
+    const fetchClimaData = async () => {
+      try {
+        const responsePromises = countries.map(country =>
+          axios.get(`https://api.weatherapi.com/v1/current.json?key=a59fc64afc414ddcb3822353233006&q=${country}&lang=es`)
+        );
+        const responses = await Promise.all(responsePromises);
+        const climaDataArray = responses.map(response => response.data);
+        setClimaData(climaDataArray);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchClimaData();
+  }, []);
+
 }
 
 export default ClimaScreen;
